e0384383b80f75b5f5851002e365314adea50933
Marryggk <99277162+Meronggk@users.noreply.github.com>
2023-08-22 18:02:51 +0100

    fixed classname

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -9,7 +9,7 @@ export const Social = () => {
         target="_blank"
         rel="noreferrer"
       >
-        <i class="uil uil-github-alt"></i>
+        <i className="uil uil-github-alt"></i>
       </a>
       <a
         href="https://www.linkedin.com/in/meron-gebremichael-8531b2243/"
@@ -17,7 +17,7 @@ export const Social = () => {
         target="_blank"
         rel="noreferrer"
       >
-        <i class="uil uil-linkedin-alt"></i>
+        <i className="uil uil-linkedin-alt"></i>
       </a>
     </div>
   );
